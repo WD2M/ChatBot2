aea2c11c4f15d93e04c10645c0b7ebd421e5e86d
WD2M <92236766+WD2M@users.noreply.github.com>
2024-01-09 09:48:35 -0500

    9/01/2024
    actualizacion del link de soporte

--- Flujos/flujos-solitarios/flowSoporte.js
+++ Flujos/flujos-solitarios/flowSoporte.js
@@ -3,8 +3,7 @@ const { addKeyword } = require('@bot-whatsapp/bot');
 const SearchContactChatWood = require('../../servicios/SearchContacs');
 
 const flowSoporte = addKeyword('4')
-    .addAnswer('Describe el problema y en un momento me comunico contigo para darte una solucion ⚙️🧰')
-    .addAnswer('https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D158022220142425&tbnid=8guoi_3WJMWCkM&vet=12ahUKEwi5ktGjiM-DAxUaEmIAHSUVClkQMygBegQIARBC..i&imgrefurl=https%3A%2F%2Fm.facebook.com%2FCreador.By.N3RY2%2F%3Flocale2%3Des_LA&docid=2AQN3oLOVCYCMM&w=854&h=369&q=chupelo&ved=2ahUKEwi5ktGjiM-DAxUaEmIAHSUVClkQMygBegQIARBC')
+    .addAnswer('Para brindarle *Soporte* a tu problema *ingresa* en el *Link* de *WhatsApp* https://wa.link/koi8pr\n\nEn el *Link* te darán pronta *Solución*  ⚙️🧰')
     .addAction({ capture: true, idle: 600000 }, async (ctx, { fallBack }) => {
 
         if (ctx?.idleFallBack) {
