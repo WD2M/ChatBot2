9dbe719bf374dc792a990625f7671a6d8a95c9be
WD2M <92236766+WD2M@users.noreply.github.com>
2024-01-28 12:14:32 -0500

    28/01

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const { createBot, createProvider, createFlow, } = require('@bot-whatsapp/bot')
 
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/json')
-const MockAdapter1 = require('@bot-whatsapp/database/json')
 const QRPortalWeb = require('@bot-whatsapp/portal')
 
 const { flowSaludo } = require('./Flujos/flujos-solitarios/flowSaludo');
@@ -13,21 +12,64 @@ const { flowMetodosDePago } = require('./Flujos/flujos-solitarios/flowMetodosDeP
 const { flowOferta } = require('./Flujos/flujos-solitarios/flowOferta');
 const { flowSoporte } = require('./Flujos/flujos-solitarios/flowSoporte');
 const { flowEntregaCuentas } = require('./Flujos/flujos-solitarios/flowEntregaCuentas');
+const { flowProductos } = require('./Flujos/flujos-solitarios/flowProductos');
 const ServerHttp = require('./Http');
 
 const adapterProvider = createProvider(BaileysProvider)
 
 const main = async () => {
-    const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowSaludo, flowPrincipalMenu, flowMetodosPago,flowFormula,flowOferta,flowMetodosDePago,flowSoporte,flowEntregaCuentas])
+    const adapterDB = new MockAdapter({filename: '1'})
+    const adapterFlow = createFlow([flowSaludo, flowPrincipalMenu, flowMetodosPago,flowFormula,flowOferta,flowMetodosDePago,flowSoporte,flowEntregaCuentas,flowProductos])
 
     await createBot({
         flow: adapterFlow,
         provider: adapterProvider,
         database: adapterDB,
     })
-    QRPortalWeb()
+    const server = new ServerHttp(adapterProvider);
+    server.start()
+}
+
+
+const main1 = async () => {
+    const adapterProvider1 = createProvider(BaileysProvider,{name: 'empresa2'})
+    const adapterDB = new MockAdapter({filename: '2'})
+    const adapterFlow = createFlow([flowSaludo, flowPrincipalMenu, flowMetodosPago,flowFormula,flowOferta,flowMetodosDePago,flowSoporte,flowEntregaCuentas,flowProductos])
+
+    await createBot({
+        flow: adapterFlow,
+        provider: adapterProvider1,
+        database: adapterDB,
+    })
+    QRPortalWeb({name: 'empresa2', port: 4001})
+}
+const main2 = async () => {
+    const adapterProvider1 = createProvider(BaileysProvider,{name: 'empresa3'})
+    const adapterDB = new MockAdapter({filename: '3'})
+    const adapterFlow = createFlow([flowSaludo, flowPrincipalMenu, flowMetodosPago,flowFormula,flowOferta,flowMetodosDePago,flowSoporte,flowEntregaCuentas,flowProductos])
+
+    await createBot({
+        flow: adapterFlow,
+        provider: adapterProvider1,
+        database: adapterDB,
+    })
+    QRPortalWeb({name: 'empresa3', port: 4002})
+}
+const main3 = async () => {
+    const adapterProvider1 = createProvider(BaileysProvider,{name: 'empresa4'})
+    const adapterDB = new MockAdapter({filename: '4'})
+    const adapterFlow = createFlow([flowSaludo, flowPrincipalMenu, flowMetodosPago,flowFormula,flowOferta,flowMetodosDePago,flowSoporte,flowEntregaCuentas,flowProductos])
+
+    await createBot({
+        flow: adapterFlow,
+        provider: adapterProvider1,
+        database: adapterDB,
+    })
+    QRPortalWeb({name: 'empresa4', port: 4003})
 }
 
 exports.adapterProvider = adapterProvider;
 main()
+main1()
+main2()
+main3()
